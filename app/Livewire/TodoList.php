93f2f2ec09a4b7921f8613a597337bc37b20de07
bellobambo <bellobambo21@gmail.com>
2023-12-21 15:37:18 +0100

    livewirecrud

---
+++ app/Livewire/TodoList.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Todo;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+
+class TodoList extends Component
+{
+
+    #[Rule('required|min:3|max:50')]
+    public $name;
+    public $search;
+
+    public function create()
+    {
+
+        $validated = $this->validateOnly('name');
+
+        Todo::create($validated);
+
+        $this->reset('name');
+
+        session()->flash('success', 'Todo Created Successfully');
+
+
+    }
+    public function render()
+    {
+
+        return view('livewire.todo-list', [
+            'todos' => Todo::latest()->get()
+        ]);
+    }
+}
